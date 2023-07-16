481a96b268434459ccac581df9ac9a4d5ba70d10
luizarn <luizadriven@gmail.com>
2023-07-16 19:45:04 -0300

    feat: new DTO and class validator

---
+++ src/dtos/createUserBodyDTO.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateUserBodyDTO {
+  @IsNotEmpty()
+  @Length(3, 100)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @Length(6, 20)
+  password: string;
+
+  @IsNotEmpty()
+  avatar: string;
+}
